576c8d65ed95ec6d5179717d9c4d91004363fff6
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2024-02-06 16:32:38 -0500

    dashboard check auth route

--- dashboard/frontend/src/components/dashboard.js
+++ dashboard/frontend/src/components/dashboard.js
@@ -1,29 +1,39 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
   const [userInfo, setUserInfo] = useState(null);
-  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-   
-      axios
-        .get("http://localhost:3001/user/info", { withCredentials: true })
-        .then((response) => {
-          setUserInfo(response.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching user info:", error);
-        });
-    
-  }, [location]);
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/auth/checkAuth`, { withCredentials: true })
+      .then(response => {
+        if (response.data.isAuthenticated) {
+          fetchUserInfo();
+        } else {
+          navigate('/');
+        }
+      })
+      .catch(error => {
+        console.error("Authentication check failed:", error);
+        navigate('/');
+      });
+  }, [navigate]);
+
+  const fetchUserInfo = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/user/info`, { withCredentials: true })
+      .then(response => {
+        setUserInfo(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching user info:", error);
+      });
+  };
 
   const filterAdminGuilds = () => {
     if (userInfo && userInfo.guilds) {
-      return userInfo.guilds.filter((guild) => {
-        return guild.permissions === 2147483647;
-      });
+      return userInfo.guilds.filter(guild => guild.permissions === 2147483647);
     }
     return [];
   };
@@ -33,26 +43,16 @@ function Dashboard() {
       {userInfo ? (
         <>
           <div className="profile-info">
-            <img
-              src={userInfo.avatar}
-              alt={`${userInfo.username}'s Avatar`}
-              style={{ width: 100, borderRadius: "50%" }}
-            />
-            <h2>
-              Welcome {userInfo.username}#{userInfo.discriminator}!
-            </h2>
+            <img src={userInfo.avatar} alt={`${userInfo.username}'s Avatar`} style={{ width: 100, borderRadius: "50%" }} />
+            <h2>Welcome {userInfo.username}#{userInfo.discriminator}!</h2>
           </div>
           <div>
             <h3>Your Admin Guilds:</h3>
             <ul className="guilds-list">
-              {filterAdminGuilds().map((guild) => (
+              {filterAdminGuilds().map(guild => (
                 <li key={guild.id} className="guild">
                   {guild.iconUrl ? (
-                    <img
-                      src={guild.iconUrl}
-                      alt={`${guild.guildName} Icon`}
-                      style={{ width: 50, borderRadius: "50%" }}
-                    />
+                    <img src={guild.iconUrl} alt={`${guild.guildName} Icon`} style={{ width: 50, borderRadius: "50%" }} />
                   ) : (
                     <div>No Icon</div>
                   )}
